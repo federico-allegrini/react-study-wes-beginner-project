1e054f09288d6bb1d2d4e1e6d4cd5739704795d8
Federico Allegrini <federicoa@mondoconvenienza.net>
2020-10-05 12:21:03 +0200

    Writing HTML with JSX

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,7 +2,15 @@ import React from "react";
 
 class StorePicker extends React.Component {
   render() {
-    return <p>Hello</p>;
+    // Comment
+    return (
+      <form className="store-selector">
+        {/* Comment */}
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store ➡</button>
+      </form>
+    );
   }
 }
 
