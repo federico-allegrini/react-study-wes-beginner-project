8e4f608809e5c32f1f31986834ff0cd86b1f1832
Federico Allegrini <federicoa@mondoconvenienza.net>
2020-10-19 11:51:48 +0200

    Persisting our state with Firebase

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,27 @@ import Order from "./Order";
 import Inventory from "./Inventory";
 import Fish from "./Fish";
 import sampleFishes from "../sample-fishes";
+import base from "../base";
+import fishes from "../sample-fishes";
 
 class App extends React.Component {
   state = {
     fishes: {},
     order: {},
   };
+
+  componentDidMount() {
+    const { params } = this.props.match;
+    this.ref = base.syncState(`${params.storeId}/fishes`, {
+      context: this,
+      state: "fishes",
+    });
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
+
   addFish = (fish) => {
     // 1. Take a copy of the existing state
     const fishes = { ...this.state.fishes };
